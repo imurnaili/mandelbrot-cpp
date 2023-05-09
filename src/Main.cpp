69de13b2a82b48a1ec8ab7fb15ba642c6ac84af5
Maximilian Rumi <imurnailimixam@gmail.com>
2023-05-09 22:30:15 +0200

    Set bg color

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main(int argc, char** argv) {
-	std::cout << "Hello World!" << std::endl;
 	glfwInit();
 	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	glfwMakeContextCurrent(window);
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
 	while (!glfwWindowShouldClose(window)) {
+		
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
