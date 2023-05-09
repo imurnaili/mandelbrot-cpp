01e926426395b9261d87f7ccf1ec9b270527fca6
Maximilian Rumi <imurnailimixam@gmail.com>
2023-05-09 22:11:10 +0200

    glfw window

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main(int argc, char** argv) {
 	std::cout << "Hello World!" << std::endl;
+	glfwInit();
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	//glfwMakeContextCurrent(window);
+	while (!glfwWindowShouldClose(window)) {
+		//glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	
+	glfwTerminate();
 }
