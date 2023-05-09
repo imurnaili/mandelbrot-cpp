3f0c2bd81bc95e1066f73d2de0f3b2241839892d
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2023-05-09 22:17:31 +0200

    added OpenGL

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,9 +5,9 @@ int main(int argc, char** argv) {
 	std::cout << "Hello World!" << std::endl;
 	glfwInit();
 	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	//glfwMakeContextCurrent(window);
+	glfwMakeContextCurrent(window);
 	while (!glfwWindowShouldClose(window)) {
-		//glfwSwapBuffers(window);
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 	
