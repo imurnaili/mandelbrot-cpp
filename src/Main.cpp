63ba022730d0fd94cf962ed47a62efd36df62b3b
Maximilian Rumi <imurnailimixam@gmail.com>
2023-05-09 23:23:55 +0200

    Shader and boiler plate

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,6 +2,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <string>
+#include "Shader.hpp"
 
 int main(int argc, char** argv) {
 	glfwInit();
@@ -9,66 +11,73 @@ int main(int argc, char** argv) {
 	glfwMakeContextCurrent(window);
 	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
+	Shader shader(
+		"#version 330 core\n"
+            "layout (location = 0) in vec3 aPos;\n"
+            "void main() {\n"
+            "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+            "}\0",
+		"#version 330 core\n"
+            "out vec4 FragColor;\n"
+            "void main() {\n"
+            "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+            "}\0");
+
 	while (!glfwWindowShouldClose(window)) {
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// shader boiler plate
-		const char* vertexShaderSource = "#version 330 core\n"
-			"layout (location = 0) in vec3 aPos;\n"
-			"void main() {\n"
-			"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-			"}\0";
-		const char* fragmentShaderSource = "#version 330 core\n"
-			"out vec4 FragColor;\n"
-			"void main() {\n"
-			"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-			"}\0";
-		// load and compile vertex shader
-		unsigned int vertexShader;
-		vertexShader = glCreateShader(GL_VERTEX_SHADER);
-		glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-		glCompileShader(vertexShader);
-		// load and compile fragment shader
-		unsigned int fragmentShader;
-		fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-		glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-		glCompileShader(fragmentShader);
-		// link shaders
-		unsigned int shaderProgram;
-		shaderProgram = glCreateProgram();
-		glAttachShader(shaderProgram, vertexShader);
-		glAttachShader(shaderProgram, fragmentShader);
-		glLinkProgram(shaderProgram);
-		// delete shaders
-		glDeleteShader(vertexShader);
-		glDeleteShader(fragmentShader);
-		// create vertex buffer
+		shader.bind();
+		// texture coordinates
+		float texCoords[] = {
+			-1.0f, -1.0f, // bottom left
+			1.0f, -1.0f, // bottom right
+			1.0f,  1.0f, // top right
+			-1.0f,  1.0f,  // top left
+			1.0f,  1.0f, // top right
+			-1.0f, -1.0f  // bottom left
+		};
+		// create texture from coordinates
+		unsigned int texture;
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		// set texture wrapping/filtering options
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // x-axis
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); // y-axis
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); // minification
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // magnification
+
+		// cover screen with two triangles
 		float vertices[] = {
-			-0.5f, -0.5f, 0.0f, // left
-			0.5f, -0.5f, 0.0f, // right
-			0.0f, 0.5f, 0.0f // top
+			// first triangle
+			-1.0f, -1.0f, 0.0f, // bottom left
+			 1.0f, -1.0f, 0.0f, // bottom right
+			 1.0f,  1.0f, 0.0f, // top right
+			// second triangle
+			-1.0f,  1.0f, 0.0f, // top left
+			 1.0f,  1.0f, 0.0f, // top right
+			 -1.0f, -1.0f, 0.0f  // bottom left
 		};
 		unsigned int VBO;
 		glGenBuffers(1, &VBO);
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		// copy vertices into buffer
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 		// create vertex array object
 		unsigned int VAO;
 		glGenVertexArrays(1, &VAO);
 		glBindVertexArray(VAO);
-		// configure vertex attributes
+		// tell OpenGL how to interpret vertex data
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		// enable vertex attribute
 		glEnableVertexAttribArray(0);
 		// draw triangle
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		// swap buffers and poll events
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-	
+
+	glfwDestroyWindow(window);
 	glfwTerminate();
 }
---
+++ src/Shader.hpp
@@ -0,0 +1,27 @@
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <string>
+
+class Shader {
+public:
+    Shader(const char* vertexShaderSource, const char* fragmentShaderSource);
+    void bind();
+    void setBool(const std::string& name, bool value);
+    void setInt(const std::string& name, int value);
+    void setFloat(const std::string& name, float value);
+    void setVec2(const std::string& name, const glm::vec2& value);
+    void setVec2(const std::string& name, float x, float y);
+    void setVec3(const std::string& name, const glm::vec3& value);
+    void setVec3(const std::string& name, float x, float y, float z);
+    void setVec4(const std::string& name, const glm::vec4& value);
+    void setVec4(const std::string& name, float x, float y, float z, float w);
+    void setMat2(const std::string& name, const glm::mat2& mat);
+    void setMat3(const std::string& name, const glm::mat3& mat);
+    void setMat4(const std::string& name, const glm::mat4& mat);
+    //destructors
+    ~Shader();
+    //copy
+    Shader(const Shader& other);
+private:
+    unsigned int ID;
+};
